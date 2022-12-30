7dd9b67c02404c2aca1471ee106eb3b2cbc44a78
navvkaur <71594057+navvkaur@users.noreply.github.com>
2022-12-30 11:11:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,29 @@ const path = require('path');
 const bodyParser=require('body-parser');
 
 const app = express();
-
+app.set('view engine','pug');
+app.set('views','viewers');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const contactus = require('./routes/contactus');
+
 const rootdir = require('./util/path');
 
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname,'Public')));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(adminRoutes);
 app.use(shopRoutes);
+app.use(contactus);
+
+app.use('/success',(req,res,next)=>
+{
+    
+    res.sendFile(path.join(__dirname,'viewers','success.html'));
+    
+    
+})
 app.use((req,res,next)=>
 {
     
